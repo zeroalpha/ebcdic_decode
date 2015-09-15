47f4757d8cc38334a83cc2fef16685ec8e1b4e83
ChristianMoniatis <christian.moniatis@deutschebahn.com>
2015-09-15 16:47:57 +0200

    first draft of FTP and Converter Classes

---
+++ lib/mvs_ftp.rb
@@ -0,0 +1,46 @@
+require 'net/ftp'
+require 'pry'
+
+class MvsFtp
+  
+  def initialize(host,login)
+    begin
+      @ftp = Net::FTP.new
+      @ftp.connect host,21
+    rescue => e
+      puts "\nFailed to connect to #{host}:21.\nReason: #{e.inspect}"
+      exit 1
+    end
+    raise LoginFailedError, "Login rejected for user #{login[0]}" unless @ftp.login(*login)    
+    @ftp.sendcmd 'SITE RDW' # Enable transfer of RDW for VB Datasets
+  end
+
+  def download(dataset)
+    #"550 Retrieval of a whole partitioned data set is not supported. Use MGET or MVSGET for this purpose.\n"
+    #binding.pry
+    ret = nil
+    begin
+      ret = @ftp.getbinaryfile(dataset)
+    rescue => e
+      if(e.message.index("550")) then
+        ret = download_member(dataset)
+      else
+        puts e.inspect
+        ret = nil
+      end
+    end
+    ret
+  end
+
+  def download_member(partitioned_dataset)
+    
+    @ftp.chdir "'#{partitioned_dataset}'"
+    member = @ftp.dir
+    member.shift #header zeile entfernen
+    member.map!{|line| line.split(" ")[0]} #den wahnsinn vom FTP auf den member namen eindampfen
+    member.map do |mem|
+      @ftp.getbinaryfile(mem)
+    end
+    binding.pry
+  end
+end
